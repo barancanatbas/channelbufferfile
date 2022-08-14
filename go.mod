99908fd35fff3b1a58858b54dffaf93f8ea64859
baran can atbas <barancanatbas102@gmail.com>
2022-08-14 21:26:22 +0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module channelbufferfile
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+var fileName = "deneme.txt"
+
+func WriteFile(item chan []byte, doneChannel chan bool, errChannel chan error) {
+	file, err := os.Create(fileName)
+	if err != nil {
+		errChannel <- err
+	}
+	for {
+		select {
+		case <-doneChannel:
+			return
+		case err := <-errChannel:
+			errChannel <- err
+		case data := <-item:
+			_, err := file.Write(data)
+			if err != nil {
+				errChannel <- err
+			}
+			errChannel <- nil
+		}
+	}
+}
+
+func main() {
+	errChannel := make(chan error)
+	doneChannel := make(chan bool)
+	item := make(chan []byte)
+
+	items := []string{"baran", " can", " atbaş"}
+
+	go WriteFile(item, doneChannel, errChannel)
+	for _, v := range items {
+		time.Sleep(time.Second)
+		item <- []byte(v)
+		err := <-errChannel
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	doneChannel <- true
+}
